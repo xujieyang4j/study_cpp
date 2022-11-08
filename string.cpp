c8759600665076f14a0233d68ac618aef878feec
曾雅佳 <cengyajia@cengyajiadeMacBook-Pro.local>
2022-11-09 00:28:36 +0800

    init

---
+++ string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string s1;
+    s1 = "s1";
+    cout<<s1<<endl;
+    string s2 = "s2";
+    cout<<s2<<endl;
+    string s3("s3");
+    cout<<s3<<endl;
+    string s4(6, 'a');
+    cout<<s4<<endl;
+    return 0;
+}
